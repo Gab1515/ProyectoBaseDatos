ad6e6ac1ed1efb2e5ed5ded5a234d8fbc5a664f0
gabriela <anaislu12@gmail.com>
2023-11-07 04:41:45 +0100

    avances

--- Index.js
+++ Index.js
@@ -1,12 +1,10 @@
- const { app, port } = require("./Src/app");
- const {Connection} = require("./Src/db");
+const { app, port } = require("./Src/app");
+const { connection } = require("./Src/db");
 
+// // /* -------SINCRONIZAMOS BASE DE DATOS Y SERVIDOR EXPRESS------------- */
 
-// /* -------SINCRONIZAMOS BASE DE DATOS Y SERVIDOR EXPRESS------------- */
-
-Connection().then(
-
-app.listen(port, () => {
+connection().then(
+  app.listen(port, () => {
     console.log(`Servidor levantado en el puerto: ${port}`);
   })
 );
--- Src/db.js
+++ Src/db.js
@@ -1,14 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose"); /* importo el módulo de mongoose */
 
-
-
-async function Connection() {
-
-     await mongoose
-     .connect('mongodb+srv://anaislu12:1234@ejemplo.tpneya6.mongodb.net/?retryWrites=true&w=majority')
-
-     .catch((err) => console.log(err));
-
+async function connection() {
+  await mongoose
+    .connect(
+     "mongodb+srv://anaislu12:1234@ejemplo.tpneya6.mongodb.net/?retryWrites=true&w=majority"
+    )
+    .then(console.log("Base de datos Mongo conectada"))
+    .catch((err) => console.log(err));
 }
 
-module.exports = { Connection };
+module.exports = { connection };
